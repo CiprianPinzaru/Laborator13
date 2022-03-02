1983aa86b3b0dc747da226162d674f3743875267
Pinzaru Ciprian <pinzaru.ciprian@elev.cihcahul.md>
2022-03-02 14:29:26 +0200

    Suma a doua numere!

--- Laborator13/Problema1/Program.cs
+++ Laborator13/Problema1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Ciprian Pinzaru, commit");
+﻿int nr1 = 10;
+int nr2 = 19;
+int suma = nr1 + nr2;
+Console.WriteLine($"{nr1}+{nr2}={suma}");
